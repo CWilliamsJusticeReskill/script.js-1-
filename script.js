aaaa0c2fd0380629e5758ea71addc9cfd4aaee13
Christopher Williams <im_CHrisW@SccColorado.og>
2020-12-21 15:05:37 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("page-top");
+document.getElementById("page-top").innerHTML = "Individuum enim habet coepi vivum non possunt plene ad angustias supergreditur enim de sensu ad singulos pertinere ad curam carnis curam latius patet Omnis homo in aliquo puncto oportet decernere, utrum et ambulabit in tenebris lumen partum altruism est perniciosius  aut sui solius amans.  Hoc judicium instat et urget vitae sit quid alii agis?(Martinius Lutherus Rex, Jr)";
+var list = document.getElementById("my-favorite-people");
+var newPerson = document.createElement("li");
+var newPersonText = document.createTextNode("Mohatma Ghandi");
+newPerson.appendChild(newPersonText);
+list.appendChild(newPerson);
+console.log(newPerson);
+
+
+
+
+
+
